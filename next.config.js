fe4bb8913cc0568692d503f2eec3eaca10cc7be0
Fray Dalton <fray117@exploiter.id>
2023-03-24 19:43:10 +0700

    add staging localhost as whitelist

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+	experimental: {
+		appDir: true,
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'http',
+				hostname: 'localhost',
+				port: '8080',
+				pathname: '/assets/img/**',
+			},
+		],
+	},
 }
 
 module.exports = nextConfig
