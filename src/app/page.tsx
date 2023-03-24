cb7f38d545f5c84396ca1002f5722456ebfc8c4c
Fray Dalton <fray117@exploiter.id>
2023-03-25 00:35:37 +0700

    move fetch api to component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,87 +1,79 @@
+import HomeHero from '@/components/HomeHero'
 import Image from 'next/image'
 import Link from 'next/link'
 
 // import styles from './page.module.css'
 
-import { CheckIcon } from '@heroicons/react/20/solid'
+import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
 
-const includedFeatures = [
-	'Private forum access',
-	'Member resources',
-	'Entry to annual conference',
-	'Official member t-shirt',
+const features = [
+
+
+	{
+		name: 'Push to deploy',
+		description:
+			'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+		icon: CloudArrowUpIcon,
+	},
+	{
+		name: 'SSL certificates',
+		description:
+			'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
+		icon: LockClosedIcon,
+	},
+	{
+		name: 'Simple queues',
+		description:
+			'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
+		icon: ArrowPathIcon,
+	},
+	{
+		name: 'Advanced security',
+		description:
+			'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+		icon: FingerPrintIcon,
+	},
 ]
 
-async function getAnimeData() {
-	const res = await fetch('http://localhost:3000/api/hello')
-
-	if (!res.ok) {
-		// This will activate the closest `error.js` Error Boundary
-		throw new Error('Failed to fetch data')
-	}
-
-	return res.json()
-}
-
 export default async function Home() {
-	const randomAnime = await getAnimeData()
-
 	return (
 		<main>
-			<div className="pb-24 sm:pb-32">
-				<div className="mx-auto max-w-7xl px-6 lg:px-8 relative">
-					<Image className="mx-auto relative z-0 h-96 object-cover" alt={randomAnime.alt} src={randomAnime.src} width={850} height={300} />
-					<div className="-mt-16 relative z-10 content-bg mx-auto max-w-2xl sm:text-center py-4 px-6 rounded-lg">
-						<h2 className="text-3xl font-bold tracking-tight text-gray-100 sm:text-4xl">Home sweet localhost</h2>
-						<p className="mt-8 text-lg leading-8 text-gray-400">
-							This is Fray local mainframe, you would not access this page unless you needed to. If you believe accessing this page through illegal acces, please <b>disconnect now</b>.
+			<div className="mx-auto max-w-7xl px-6 lg:px-8 relative">
+				<HomeHero />
+				<div className="-mt-16 relative z-10 content-bg mx-auto max-w-2xl sm:text-center py-4 px-6 rounded-lg">
+					<h2 className="text-3xl font-bold tracking-tight dark:text-gray-100 text-gray-900 sm:text-4xl">Home sweet localhost</h2>
+					<p className="mt-8 text-lg leading-8 dark:text-gray-400 text-gray-600">
+						This is Fray local mainframe, you would not access this page unless you needed to. If you believe accessing this page through illegal acces, please <b>disconnect now</b>.
+					</p>
+				</div>
+				<div className="py-24 sm:py-32">
+					<div className="mx-auto max-w-2xl lg:text-center">
+						<h2 className="text-base font-semibold leading-7 dark:text-lime-600 text-rose-600">Deploy faster</h2>
+						<p className="mt-2 text-3xl font-bold tracking-tight dark:text-gray-100 text-gray-900 sm:text-4xl">
+							Everything you need to deploy your app
+						</p>
+						<p className="mt-6 text-lg leading-8 dark:text-gray-400 text-gray-600">
+							Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
+							pulvinar et feugiat blandit at. In mi viverra elit nunc.
 						</p>
 					</div>
-					<div className="bg-white mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
-						<div className="p-8 sm:p-10 lg:flex-auto">
-							<h3 className="text-2xl font-bold tracking-tight text-gray-900">Lifetime membership</h3>
-							<p className="mt-6 text-base leading-7 text-gray-600">
-								Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis
-								repellendus etur quidem assumenda.
-							</p>
-							<div className="mt-10 flex items-center gap-x-4">
-								<h4 className="flex-none text-sm font-semibold leading-6 dark:text-lime-600 text-rose-600">What’s included</h4>
-								<div className="h-px flex-auto bg-gray-100" />
-							</div>
-							<ul
-								role="list"
-								className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
-							>
-								{includedFeatures.map((feature) => (
-									<li key={feature} className="flex gap-x-3">
-										<CheckIcon className="h-6 w-5 flex-none dark:text-lime-600 text-rose-600" aria-hidden="true" />
-										{feature}
-									</li>
-								))}
-							</ul>
-						</div>
-						<div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
-							<div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
-								<div className="mx-auto max-w-xs px-8">
-									<p className="text-base font-semibold text-gray-600">Pay once, own it forever</p>
-									<p className="mt-6 flex items-baseline justify-center gap-x-2">
-										<span className="text-5xl font-bold tracking-tight text-gray-900">$349</span>
-										<span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
-									</p>
-									<a
-										href="#"
-										className="mt-10 block w-full rounded-md dark:bg-lime-600 bg-rose-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:dark:bg-lime-500 bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:dark:outline-lime-600 outline-rose-600"
-									>
-										Get access
-									</a>
-									<p className="mt-6 text-xs leading-5 text-gray-600">
-										Invoices and receipts available for easy company reimbursement
-									</p>
+					<div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+						<dl className="grid max-w-xl grid-cols-1 gap-y-10 gap-x-8 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+							{features.map((feature) => (
+								<div key={feature.name} className="relative pl-16">
+									<dt className="text-base font-semibold leading-7 dark:text-gray-100 text-gray-900">
+										<div className="absolute top-0 left-0 flex h-10 w-10 items-center justify-center rounded-lg dark:bg-lime-600 bg-rose-600">
+											<feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+										</div>
+										{feature.name}
+									</dt>
+									<dd className="mt-2 text-base leading-7 dark:text-gray-400 text-gray-600">{feature.description}</dd>
 								</div>
-							</div>
-						</div>
+							))}
+						</dl>
 					</div>
 				</div>
+				{/* Component Here */}
 			</div>
 		</main>
 	)
