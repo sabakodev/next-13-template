e0768167303da02d79226a7a7f982a877778dc20
Fray Dalton <fray117@exploiter.id>
2023-03-24 18:49:19 +0700

    media queries

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,12 +30,12 @@ export default function RootLayout({
 				</div>
 				<footer className="px-6 lg:px-8 py-8 w-full">
 					<div className="mx-auto max-w-7xl px-6 lg:px-8 h-full grid grid-cols-2 content-center text-sm">
-						<span>Made heartless by Fabiano &copy; 2023</span>
-						<div className="justify-self-end grid grid-cols-4 gap-4">
-							<Link className="hover:text-gray-200" href="/about">About</Link>
-							<Link className="hover:text-gray-200" href="/projects">Projects</Link>
-							<Link className="hover:text-gray-200" href="/utility">Utility</Link>
-							<Link className="hover:text-gray-200" href="/contact">Contact</Link>
+						<span className="col-span-2 md:col-span-1">Made heartless by Fabiano &copy; 2023</span>
+						<div className="col-span-2 md:col-span-1 justify-self-end grid grid-cols-4 gap-4">
+							<Link className="hover:text-gray-200 col-span-4 sm:col-span-1" href="/about">About</Link>
+							<Link className="hover:text-gray-200 col-span-4 sm:col-span-1" href="/projects">Projects</Link>
+							<Link className="hover:text-gray-200 col-span-4 sm:col-span-1" href="/utility">Utility</Link>
+							<Link className="hover:text-gray-200 col-span-4 sm:col-span-1" href="/contact">Contact</Link>
 						</div>
 					</div>
 				</footer>
