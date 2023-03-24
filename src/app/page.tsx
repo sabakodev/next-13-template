726422a53ce0d7de63b2ef4531fc0b9e9aa0dec7
Fray Dalton <fray117@exploiter.id>
2023-03-24 16:36:42 +0700

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,75 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Link from 'next/link'
 
-const inter = Inter({ subsets: ['latin'] })
+// import styles from './page.module.css'
+
+import { CheckIcon } from '@heroicons/react/20/solid'
+
+const includedFeatures = [
+	'Private forum access',
+	'Member resources',
+	'Entry to annual conference',
+	'Official member t-shirt',
+]
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<main>
+			<div className=" py-24 sm:py-32">
+				<div className="mx-auto max-w-7xl px-6 lg:px-8">
+					<div className="mx-auto max-w-2xl sm:text-center">
+						<h2 className="text-3xl font-bold tracking-tight text-gray-100 sm:text-4xl">Simple no-tricks pricing</h2>
+						<p className="mt-6 text-lg leading-8 text-gray-400">
+							Distinctio et nulla eum soluta et neque labore quibusdam. Saepe et quasi iusto modi velit ut non voluptas
+							in. Explicabo id ut laborum.
+						</p>
+					</div>
+					<div className="bg-white mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+						<div className="p-8 sm:p-10 lg:flex-auto">
+							<h3 className="text-2xl font-bold tracking-tight text-gray-900">Lifetime membership</h3>
+							<p className="mt-6 text-base leading-7 text-gray-600">
+								Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis
+								repellendus etur quidem assumenda.
+							</p>
+							<div className="mt-10 flex items-center gap-x-4">
+								<h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">What’s included</h4>
+								<div className="h-px flex-auto bg-gray-100" />
+							</div>
+							<ul
+								role="list"
+								className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
+							>
+								{includedFeatures.map((feature) => (
+									<li key={feature} className="flex gap-x-3">
+										<CheckIcon className="h-6 w-5 flex-none text-indigo-600" aria-hidden="true" />
+										{feature}
+									</li>
+								))}
+							</ul>
+						</div>
+						<div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+							<div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+								<div className="mx-auto max-w-xs px-8">
+									<p className="text-base font-semibold text-gray-600">Pay once, own it forever</p>
+									<p className="mt-6 flex items-baseline justify-center gap-x-2">
+										<span className="text-5xl font-bold tracking-tight text-gray-900">$349</span>
+										<span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
+									</p>
+									<a
+										href="#"
+										className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+									>
+										Get access
+									</a>
+									<p className="mt-6 text-xs leading-5 text-gray-600">
+										Invoices and receipts available for easy company reimbursement
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+	)
 }
