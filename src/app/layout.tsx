04c236c2f7965e89757d7a8271ee4fd47cea82e9
Fray Dalton <fray117@exploiter.id>
2023-03-25 00:34:06 +0700

    fix color states

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,13 +29,13 @@ export default function RootLayout({
 					{children}
 				</div>
 				<footer className="px-6 lg:px-8 py-8 w-full">
-					<div className="mx-auto max-w-7xl px-6 lg:px-8 h-full grid grid-cols-2 content-center text-sm">
-						<span className="col-span-2 md:col-span-1">Made heartless by Fabiano &copy; 2023</span>
-						<div className="col-span-2 md:col-span-1 justify-self-end grid grid-cols-4 gap-4">
-							<Link className="hover:text-gray-200 col-span-4 sm:col-span-1" href="/about">About</Link>
-							<Link className="hover:text-gray-200 col-span-4 sm:col-span-1" href="/projects">Projects</Link>
-							<Link className="hover:text-gray-200 col-span-4 sm:col-span-1" href="/utility">Utility</Link>
-							<Link className="hover:text-gray-200 col-span-4 sm:col-span-1" href="/contact">Contact</Link>
+					<div className="mx-auto max-w-7xl px-6 lg:px-8 h-full grid grid-cols-2 content-center text-sm gap-8">
+						<span className="col-span-2 sm:col-span-1 text-gray-500">Made heartless by Fabiano &copy; 2023</span>
+						<div className="col-span-2 sm:col-span-1 sm:justify-self-end sm:content-end grid grid-cols-4 gap-8 sm:gap-4">
+							<Link className="dark:hover:text-gray-700 hover:text-gray-300 col-span-4 md:col-span-1" href="/about">About</Link>
+							<Link className="dark:hover:text-gray-700 hover:text-gray-300 col-span-4 md:col-span-1" href="/projects">Projects</Link>
+							<Link className="dark:hover:text-gray-700 hover:text-gray-300 col-span-4 md:col-span-1" href="/utility">Utility</Link>
+							<Link className="dark:hover:text-gray-700 hover:text-gray-300 col-span-4 md:col-span-1" href="/contact">Contact</Link>
 						</div>
 					</div>
 				</footer>
