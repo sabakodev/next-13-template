b9fb230059ab99544ed3c2d563152cb580d13180
Fray Dalton <fray117@exploiter.id>
2023-03-24 19:43:23 +0700

    add random anime generator

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,37 @@
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+	const animelist = [
+		{
+			src: 'http://localhost:8080/assets/img/Eromanga.png',
+			alt: 'Sagiri'
+		},
+		{
+			src: 'http://localhost:8080/assets/img/Kurisu.jpg',
+			alt: 'Kurisu'
+		},
+		{
+			src: 'http://localhost:8080/assets/img/Okabe-Kurisu.jpg',
+			alt: 'Okabe & Kurisu'
+		},
+		{
+			src: 'http://localhost:8080/assets/img/Sailor-Moon.jg',
+			alt: 'Usagi'
+		},
+		{
+			src: 'http://localhost:8080/assets/img/Hishiro.png',
+			alt: 'Hishiro'
+		},
+		{
+			src: 'http://localhost:8080/assets/img/Moe.png',
+			alt: 'Loli'
+		},
+		{
+			src: 'http://localhost:8080/assets/img/Rui.jpg',
+			alt: 'Rui'
+		},
+
+	]
+
+	const randomAnime = animelist[Math.floor(Math.random() * animelist.length)]
+
+	return new Response(JSON.stringify(randomAnime))
 }
