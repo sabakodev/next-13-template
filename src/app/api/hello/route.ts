db3186ad3092275c835127a3b27a497df2f19341
Fray Dalton <fray117@exploiter.id>
2023-03-26 12:29:33 +0700

    fix routing

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,45 +1,6 @@
-import EromangaImage from '../../../../public/img/Eromanga.png'
-import KurisuImage from '../../../../public/img/Kurisu.jpg'
-import OkabeKurisuImage from '../../../../public/img/Okabe-Kurisu.jpg'
-import SailorMoonImage from '../../../../public/img/Sailor-Moon.jpg'
-import HishiroImage from '../../../../public/img/Hishiro.png'
-import MoeImage from '../../../../public/img/Moe.png'
-import RuiImage from '../../../../public/img/Rui.jpg'
-
 export async function GET(request: Request) {
-	const animelist = [
-		{
-			src: EromangaImage,
-			alt: 'Sagiri'
-		},
-		{
-			src: KurisuImage,
-			alt: 'Kurisu'
-		},
-		{
-			src: OkabeKurisuImage,
-			alt: 'Okabe & Kurisu'
-		},
-		{
-			src: SailorMoonImage,
-			alt: 'Usagi'
-		},
-		{
-			src: HishiroImage,
-			alt: 'Hishiro'
-		},
-		{
-			src: MoeImage,
-			alt: 'Loli'
-		},
-		{
-			src: RuiImage,
-			alt: 'Rui'
-		},
-
-	]
-
-	const randomAnime = animelist[Math.floor(Math.random() * animelist.length)]
-
-	return new Response(JSON.stringify(randomAnime))
+	return new Response(JSON.stringify({
+		code: 200,
+		message: 'Hello world',
+	}))
 }
