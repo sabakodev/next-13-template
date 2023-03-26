2fc45ae17b5b5e4dfb605df82b1d5037528ac251
Fray Dalton <fray117@exploiter.id>
2023-03-26 12:27:42 +0700

    remove unnecessary clutter

--- src/app/utility/page.tsx
+++ src/app/utility/page.tsx
@@ -6,35 +6,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import ActionCard from '@/components/ActionCard'
 
-const callouts = [
-	{
-		name: 'Desk and Office',
-		description: 'Work from home accessories',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-		imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
-		href: '#',
-	},
-	{
-		name: 'Self-Improvement',
-		description: 'Journals and note-taking',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
-		imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
-		href: '#',
-	},
-	{
-		name: 'Travel',
-		description: 'Daily commute essentials',
-		imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
-		imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
-		href: '#',
-	},
-]
-
 const utilitiesTool = {
 	title: 'phpMyAdmin',
 	description: 'Manage database and beyond from a click at ease without worrying any unreadable formatting.',
 	action: 'https://localhost',
 	docs: 'https://docs.phpmyadmin.net/en/latest/',
+	img: 'https://www.phpmyadmin.net/static/images/screenshots/designer.png'
 }
 
 export default function Utility() {
@@ -53,6 +30,7 @@ export default function Utility() {
 						description={utilitiesTool.description}
 						action={utilitiesTool.action}
 						docs={utilitiesTool.docs}
+						img={utilitiesTool.img}
 					/>
 				</div>
 			</div>
